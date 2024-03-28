eec7c6a72d52501df721de2206d48bbb4befd0ae
FlameBox <flameboxdev@ya.ru>
2024-03-28 22:51:05 +0300

    222

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,16 +3,17 @@ import './Form.css';
 import {useTelegram} from "../../hooks/useTelegram";
 
 const Form = () => {
-    const [country, setCountry] = useState('');
-    const [street, setStreet] = useState('');
+    const [firstname, setFirstname] = useState('');
+    const [nname, setName] = useState('');
+    const [lastname, setLastname] = useState('');
     const [subject, setSubject] = useState('physical');
     const {tg} = useTelegram();
 
     const onSendData = useCallback(() => {
         const data = {
-            country,
-            street,
-            subject
+            firstname,
+            nname,
+            lastname
         }
         tg.sendData(JSON.stringify(data));
     }, [country, street, subject])
@@ -26,47 +27,58 @@ const Form = () => {
 
     useEffect(() => {
         tg.MainButton.setParams({
-            text: 'Отправить данные'
+            text: 'Составить договор'
         })
     }, [])
 
     useEffect(() => {
-        if(!street || !country) {
+        if(!firstname ) {
             tg.MainButton.hide();
         } else {
             tg.MainButton.show();
         }
-    }, [country, street])
+    }, [firstname])
 
-    const onChangeCountry = (e) => {
-        setCountry(e.target.value)
+    const onChangeFirstname = (e) => {
+        setFirstname(e.target.value)
     }
 
-    const onChangeStreet = (e) => {
-        setStreet(e.target.value)
+    const onChangeName = (e) => {
+        setName(e.target.value)
     }
 
+    const onChangeLastname = (e) => {
+        setLastname(e.target.value)
+    }
     const onChangeSubject = (e) => {
         setSubject(e.target.value)
     }
 
     return (
         <div className={"form"}>
-            <h3>Введите ваши данные</h3>
+            <h3>Введите данные</h3>
             <input
                 className={'input'}
                 type="text"
-                placeholder={'Страна'}
-                value={country}
-                onChange={onChangeCountry}
+                placeholder={'Фамилия'}
+                value={firstname}
+                onChange={onChangeFirstname}
             />
             <input
                 className={'input'}
                 type="text"
-                placeholder={'Улица'}
-                value={street}
-                onChange={onChangeStreet}
+                placeholder={'Имя'}
+                value={nname}
+                onChange={onChangeName}
             />
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Имя'}
+                value={lastname}
+                onChange={onChangeLastname}
+            />
+
             <select value={subject} onChange={onChangeSubject} className={'select'}>
                 <option value={'physical'}>Физ. лицо</option>
                 <option value={'legal'}>Юр. лицо</option>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,9 +9,7 @@ const Header = () => {
     return (
         <div className={'header'}>
             <Button onClick={onClose}>Закрыть</Button>
-            <span className={'username'}>
-                {user?.username}
-            </span>
+
         </div>
     );
 };
